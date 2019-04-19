763036daafaed89f47bdca4245b3b36ad8ceb6a9
markklik <markklik@gmail.com>
2019-04-19 23:39:28 +0200

    License headers

--- fst/tests/test_fst_version.py
+++ fst/tests/test_fst_version.py
@@ -1,3 +1,24 @@
+#  fst-format - Python package for ultra fast storage and retrieval of datasets
+#
+#  Copyright (C) 2019-present, Mark AJ Klik
+#
+#  This file is part of the fst-format Python package.
+#
+#  The fst-format Python package is free software: you can redistribute it
+#  and/or modify it under the terms of the GNU Affero General Public License
+#  version 3 as published by the Free Software Foundation.
+#
+#  The fst-format Python package is distributed in the hope that it will be
+#  useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
+#  General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with the fst-format Python package. If not, see
+#  <http://www.gnu.org/licenses/>.
+#
+#  You can contact the author at:
+#  - fst-format source repository: https://github.com/fstpackage/fst-format
 
 import fst
 
