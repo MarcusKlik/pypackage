d537ca632a3fd7452ac19847ff08703f2660a99a
markklik <markklik@gmail.com>
2019-04-19 23:20:58 +0200

    Test package version number

--- fst/tests/test_fst_version.py
+++ fst/tests/test_fst_version.py
@@ -6,3 +6,7 @@ class Test_fst_version(object):
     def test_returns_string(self):
         v = fst.fst_version()
         assert isinstance(v, str)
+
+    def test_correct_version(self):
+        v = fst.fst_version()
+        assert v == "0.0.1"
